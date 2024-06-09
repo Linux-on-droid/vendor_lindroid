01f8e0ea283afffe226b3098062f4d367375b68c
nift4 <nift4@protonmail.com>
2024-06-09 14:57:58 +0200

    deprecate rootfs from /system

--- lxc/lxc.mk
+++ lxc/lxc.mk
@@ -11,11 +11,5 @@ PRODUCT_COPY_FILES += $(LOCAL_PATH)/default.conf:$(TARGET_COPY_OUT_SYSTEM)/lindr
 # Default container
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/container/default/config:$(TARGET_COPY_OUT_SYSTEM)/lindroid/lxc/container/default/config
 
-# Default container rootfs tarball
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/container/default/rootf.tar.gz:$(TARGET_COPY_OUT_SYSTEM)/lindroid/lxc/container/default/rootfs.tar.gz
-
 # Lindroid Init
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/init.lindroid.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.lindroid.rc
-
-# Lindroid container setup script
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/lindroid_container_setup.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/lindroid_container_setup.sh
