1abe08ae7b897ffaa56177d3ea3387ed78e38f5c
Erfan Abdi <erfangplus@gmail.com>
2024-06-13 07:25:09 +0330

    app: Make SecondaryDisplay full screen

--- app/app/src/main/java/org/lindroid/ui/SecondaryDisplayActivity.java
+++ app/app/src/main/java/org/lindroid/ui/SecondaryDisplayActivity.java
@@ -1,5 +1,6 @@
 package org.lindroid.ui;
 
+import static android.view.View.SYSTEM_UI_FLAG_FULLSCREEN;
 import static org.lindroid.ui.Constants.CONTAINER_NAME;
 
 import android.os.Bundle;
@@ -8,6 +9,7 @@ public class SecondaryDisplayActivity extends DisplayActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(SYSTEM_UI_FLAG_FULLSCREEN);
 
         start(1, CONTAINER_NAME);
     }
