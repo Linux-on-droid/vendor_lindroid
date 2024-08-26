a4d658c58c0163a41aba5dc05d80f2fe764f6757
nift4 <nift4@protonmail.com>
2024-08-26 19:59:45 +0200

    working on list ui

--- app/app/src/main/java/org/lindroid/ui/LauncherActivity.java
+++ app/app/src/main/java/org/lindroid/ui/LauncherActivity.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -21,81 +22,164 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LauncherActivity extends Activity {
     private static final String TAG = "LauncherActivity";
+    private static final int SAF_ROOTFS_REQUEST = 1000;
+    private String mNewName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.launcher);
         new Thread(() -> {
             String runningContainer = ContainerManager.isAtLeastOneRunning();
             if (runningContainer != null) {
                 runOnUiThread(() -> startDisplayActivitiesOnAllDisplays(runningContainer));
                 return;
             }
-            List<String> containers = ContainerManager.listContainers();
-            List<Boolean> running = containers.stream().map(ContainerManager::isRunning)
-                    .collect(Collectors.toUnmodifiableList());
-            runOnUiThread(() -> {
-                setContentView(R.layout.launcher);
-                ((RecyclerView) findViewById(R.id.recycler)).setAdapter(new RecyclerView.Adapter<ViewHolder>() {
-                    @NonNull
-                    @Override
-                    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                        if (viewType == 1) {
-                            return new AddViewHolder(LayoutInflater.from(LauncherActivity.this)
-                                    .inflate(R.layout.add_entry, parent, false));
-                        }
-                        return new ContainerViewHolder(LayoutInflater.from(LauncherActivity.this)
-                                .inflate(R.layout.container_entry, parent, false));
-                    }
+            updateAdapter();
+        }).start();
+    }
 
-                    @Override
-                    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-                        if (holder instanceof ContainerViewHolder cvh) {
-                            cvh.mContainerName.setText(containers.get(position));
-                            cvh.setRunning(running.get(position));
-                            cvh.onClick = () -> {
-                                // TODO start/stop
-                            };
-                        } else if (holder instanceof AddViewHolder avh) {
-                            avh.onClick = () -> {
-                                // TODO ask for name
-                                // TODO download/pick file
-                                if (!ContainerManager.listContainers().contains("default"))
-                                    createContainer("default",
-                                        Uri.fromFile(new File(getFilesDir(), "rootfs.tar.gz")));
-                            };
-                        }
+    private void updateAdapter() {
+        List<String> containers = ContainerManager.listContainers();
+        List<Boolean> running = containers.stream().map(ContainerManager::isRunning)
+                .collect(Collectors.toUnmodifiableList());
+        runOnUiThread(() -> {
+            Log.i(TAG, "Setting adapter with container count " + containers.size());
+            ((RecyclerView) findViewById(R.id.recycler)).setAdapter(new RecyclerView.Adapter<ViewHolder>() {
+                @NonNull
+                @Override
+                public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                    if (viewType == 1) {
+                        return new AddViewHolder(LayoutInflater.from(LauncherActivity.this)
+                                .inflate(android.R.layout.simple_list_item_1, parent, false));
                     }
+                    return new ContainerViewHolder(LayoutInflater.from(LauncherActivity.this)
+                            .inflate(R.layout.container_entry, parent, false));
+                }
 
-                    @Override
-                    public void onViewRecycled(@NonNull ViewHolder holder) {
-                        if (holder instanceof ContainerViewHolder cvh) {
-                            cvh.onClick = null;
-                        } else if (holder instanceof AddViewHolder avh) {
-                            avh.onClick = null;
-                        }
+                @Override
+                public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+                    if (holder instanceof ContainerViewHolder cvh) {
+                        cvh.mContainerName.setText(containers.get(position));
+                        cvh.setRunning(running.get(position));
+                        cvh.onClick = () -> {
+                            // TODO start/stop
+                        };
+                    } else if (holder instanceof AddViewHolder avh) {
+                        avh.onClick = () -> {
+                            // TODO ask for name
+                            String name = "default";
+                            if (!ContainerManager.listContainers().contains(name)) {
+                                mNewName = name;
+                                /*Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                                intent.setType("*\/*");
+                                startActivityForResult(intent, SAF_ROOTFS_REQUEST);*/
+                                createContainer(name, Uri.parse("https://github.com/Linux-on-droid/lindroid-rootfs/releases/download/nightly/lindroid-rootfs-arm64-plasma.zip.tar.gz"));
+                            } else {
+                                // TODO duplicate warning
+                            }
+                        };
                     }
+                }
 
-                    @Override
-                    public int getItemCount() {
-                        return containers.size();
+                @Override
+                public void onViewRecycled(@NonNull ViewHolder holder) {
+                    if (holder instanceof ContainerViewHolder cvh) {
+                        cvh.onClick = null;
+                    } else if (holder instanceof AddViewHolder avh) {
+                        avh.onClick = null;
                     }
+                }
 
-                    @Override
-                    public int getItemViewType(int position) {
-                        return position == getItemCount() - 1 ? 1 : 0;
-                    }
-                });
+                @Override
+                public int getItemCount() {
+                    return containers.size() + 1;
+                }
+
+                @Override
+                public int getItemViewType(int position) {
+                    return position == getItemCount() - 1 ? 1 : 0;
+                }
             });
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SAF_ROOTFS_REQUEST) {
+            if (resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+                createContainer(mNewName, data.getData());
+            } else {
+                Toast.makeText(this, R.string.saf_pick_failed, Toast.LENGTH_LONG).show();
+            }
+            mNewName = null;
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     private void createContainer(String containerName, Uri rootfs) {
+        if ("http".equals(rootfs.getScheme()) || "https".equals(rootfs.getScheme())) {
+            View v = LayoutInflater.from(this).inflate(R.layout.progressdialog, null);
+            final TextView tv = v.findViewById(R.id.prog_message);
+            tv.setText(R.string.dl_connecting);
+            AlertDialog inner = new MaterialAlertDialogBuilder(this)
+                    .setCancelable(false)
+                    .setTitle(R.string.downloading)
+                    .setView(v)
+                    .show();
+            new Thread(() -> {
+                File f = new File(getCacheDir(), String.valueOf(System.currentTimeMillis()));
+                try {
+                    HttpURLConnection conn = (HttpURLConnection) new URL(rootfs.toString()).openConnection();
+                    final long contentLength = conn.getContentLengthLong();
+                    long readBytes = 0;
+                    long lastReportedReadBytes = 0;
+                    final byte[] b = new byte[1024 * 1024];
+                    try (InputStream s = conn.getInputStream()) {
+                        try (OutputStream s2 = new FileOutputStream(f)) {
+                            int r;
+                            while ((r = s.read(b)) != -1) {
+                                s2.write(b, 0, r);
+                                if (r >= 0) readBytes += r;
+                                if (lastReportedReadBytes < readBytes - 1024 * 1024) {
+                                    lastReportedReadBytes = readBytes;
+                                    final long rb = readBytes;
+                                    runOnUiThread(() -> tv.setText(getString(R.string.progress,
+                                            rb / (1024 * 1024),
+                                            contentLength / (1024 * 1024))));
+                                }
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, Log.getStackTraceString(e));
+                    if (f.exists() && !f.delete())
+                        Log.e(TAG, "failed to delete temp file");
+                    runOnUiThread(() ->
+                        Toast.makeText(this, R.string.download_failed, Toast.LENGTH_LONG).show()
+                    );
+                    return;
+                } finally {
+                    runOnUiThread(inner::dismiss);
+                }
+                runOnUiThread(() -> createContainer(containerName, Uri.fromFile(f)));
+            });
+            return;
+        }
         View v = LayoutInflater.from(this).inflate(R.layout.progressdialog, null);
         ((TextView) v.findViewById(R.id.prog_message)).setText(R.string.creating_container_message);
         AlertDialog inner = new MaterialAlertDialogBuilder(this)
@@ -105,7 +189,10 @@ public class LauncherActivity extends Activity {
                 .show();
         new Thread(() -> {
             if (ContainerManager.addContainer(containerName, getContentResolver(), rootfs)) {
-                startOrError(containerName, inner::dismiss);
+                startOrError(containerName, () -> {
+                    updateAdapter();
+                    inner.dismiss();
+                });
             } else {
                 runOnUiThread(() -> new MaterialAlertDialogBuilder(LauncherActivity.this)
                         .setTitle(R.string.failed_to_create)
@@ -188,6 +275,7 @@ public class LauncherActivity extends Activity {
         public AddViewHolder(View root) {
             super(root);
             root.setOnClickListener(this);
+            ((TextView) root).setText(R.string.add_container);
         }
 
         @Override
